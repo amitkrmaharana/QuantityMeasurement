b8e98f8899c52c4e83eb36fd201a0e878048db94
amitkrmaharana <ak657harley@gmail.com>
2021-05-04 16:56:41 +0530

    Refactored: returned null if an null entry for length has been given

--- quantity_measurement.py
+++ quantity_measurement.py
@@ -1,11 +1,31 @@
+from logger import *
+
+
 class QuantityMeasurement:
 
     def compare_length(self, unit1, unit2, quantity1, quantity2):
-        if unit1 == "ft":
-            quantity1 *= 12
-        if unit2 == "ft":
-            quantity2 *= 12
-        if quantity1 == quantity2:
-            return True
-        else:
-            return False
+        """
+
+        :param unit1: unit of 1st length
+        :param unit2: unit of 2nd length
+        :param quantity1: 1st length
+        :param quantity2: 2nd length
+        :return: equality of the lengths if both are not null
+        """
+        try:
+            if quantity1 is not None and quantity2 is not None:
+                if unit1 == "ft":
+                    quantity1 *= 12
+                if unit2 == "ft":
+                    quantity2 *= 12
+                if quantity1 == quantity2:
+                    return True
+                else:
+                    return False
+            elif quantity1 is None:
+                return None
+            elif quantity2 is None:
+                return None
+            return 0
+        except Exception as e:
+            logger.error(e)
