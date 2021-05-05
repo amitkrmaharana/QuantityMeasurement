fd7b22ac78d79f1ba38f43624502bf5a0c8b514c
amitkrmaharana <ak657harley@gmail.com>
2021-05-05 08:20:10 +0530

    Added: added a test case to fail 1 ft i equal to 1 in

--- quantity_measurement_test.py
+++ quantity_measurement_test.py
@@ -25,6 +25,9 @@ class TestQuantityMeasurement(unittest.TestCase):
     def test_lengths_given_0_ft_and_0_in_should_compare_and_return_true(self):
         self.assertTrue(QuantityMeasurement().compare_length("ft", "in", 0, 0))
 
+    def test_lengths_given_1_ft_and_1_in_should_compare_and_return_false(self):
+        self.assertFalse(QuantityMeasurement().compare_length("ft", "in", 1, 1))
+
 
 if __name__ == '__main__':
     unittest.main()
