d6d5ccf2725c0971f823c4f0043ae06969cb7c0b
amitkrmaharana <ak657harley@gmail.com>
2021-05-05 10:10:37 +0530

    Added: added a test case to compare 1 ft and 1 yd to be false

--- quantity_measurement_test.py
+++ quantity_measurement_test.py
@@ -43,6 +43,9 @@ class TestQuantityMeasurement(unittest.TestCase):
     def test_lengths_given_3_ft_and_1_yd_should_compare_and_return_true(self):
         self.assertTrue(QuantityMeasurement().compare_length("ft", "yd", 3, 1))
 
+    def test_lengths_given_1_ft_and_1_yd_should_compare_and_return_false(self):
+        self.assertFalse(QuantityMeasurement().compare_length("ft", "yd", 1, 1))
+
 
 if __name__ == '__main__':
     unittest.main()
